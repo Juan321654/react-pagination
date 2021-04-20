adb184c1834a3479067d853a95ee680a6e01ca16
Juan Camacho <juanc@tiv.solutions>
2021-04-20 10:05:31 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
+import "./App.css";
 
-function App() {
+const PER_PAGE = 10;
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function fetchData() {
+    fetch("https://ihsavru.me/Demo/uploads.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const {
+          course: { uploads }
+        } = data;
+        setData(uploads);
+      });
+  }
+
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
+
+  const offset = currentPage * PER_PAGE;
+
+  const currentPageData = data
+    .slice(offset, offset + PER_PAGE)
+    .map(({ thumburl }) => <img src={thumburl} />);
+
+  const pageCount = Math.ceil(data.length / PER_PAGE);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Paginate Example</h1>
+      {currentPageData}
+      <ReactPaginate
+        previousLabel={"← Previous"}
+        nextLabel={"Next →"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
+      />
     </div>
   );
 }
-
-export default App;
